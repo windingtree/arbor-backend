c759e88fe31ae137cc83c406f44592707fa9f748
Oleg Kudrenko <oleg.kudrenko@gmail.com>
2020-02-16 23:54:45 +0200

    start with files upload

--- modules/orgids_json/lib/impl.js
+++ modules/orgids_json/lib/impl.js
@@ -19,15 +19,26 @@ module.exports = function (config) {
     };
 
     const saveJson = async (address, orgidJson, baseUrl) => {
-        console.log('saveJson', address, 'orgidJson...');
+        log.debug('saveJson', address, 'orgidJson...');
         const orgidJsonString = JSON.stringify(orgidJson, null, 2);
         const dir = `uploads/${address}/${orgidJson.id ? `${orgidJson.id}/` : ''}`;
-        const file = `${orgidJson.id ? '' : 'wizard-'}0x${keccak256(orgidJsonString)}.json`;
-        await writeFile(dir, file, orgidJsonString);
-        return `${baseUrl}${dir}${file}`;
+        const fileName = `${orgidJson.id ? '' : 'wizard-'}0x${keccak256(orgidJsonString)}.json`;
+        await writeFile(dir, fileName, orgidJsonString);
+        return `${baseUrl}${dir}${fileName}`;
+    };
+
+    const saveMedia =  async (mediaType, options, baseUrl) => {
+        const { address, file, id } = options;
+        log.debug(`saveMedia(${mediaType}, ${address}), file, baseUrl`);
+        const orgidJsonString = JSON.stringify(file, null, 2);
+        const dir = `uploads/${address}/mediaType/${id ? id : 'wizard'}/`;
+        const fileName = `___.jpg`; //${keccak256(file)}
+        await writeFile(dir, fileName, orgidJsonString);
+        return `${baseUrl}${dir}${fileName}`;
     };
 
     return Promise.resolve({
-        saveJson
+        saveJson,
+        saveMedia
     });
 };
--- modules/orgids_json/lib/rest.js
+++ modules/orgids_json/lib/rest.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/tmp/' });
 const url = require('url');
 const filenameSplitted = __filename.split(__filename[0]);
 const log = require("log4js").getLogger(`${filenameSplitted[filenameSplitted.length - 3]}/${filenameSplitted[filenameSplitted.length - 1].replace('.js', '')}`);
 log.level = 'debug';
 
 module.exports = function (rest, orgids_json) {
+    const baseUrl = (req) =>  url.format({ protocol: req.protocol, host: req.get('host'), pathname: '/' });
+
     router.post('/json', async (req, res) => {
         const { address, orgidJson } = req.body;
-        console.log('POST:JSON', req.body, req.params);
         try {
-            const uri = await orgids_json.saveJson(address, orgidJson, url.format({ protocol: req.protocol, host: req.get('host'), pathname: '/' }));
-            console.log('uri', uri);
+            const uri = await orgids_json.saveJson(address, orgidJson, baseUrl(req));
+            log.debug('json saved. uri', uri);
             const json = {
                 data: {
-                    type: 'url',
+                    type: 'uri',
+                    uri
+                }
+            };
+            res.status(200).send(json)
+        } catch (e) {
+            const {code, json} = rest.decorateError(e);
+            return res.status(code).send(json)
+        }
+    });
+
+    router.post('/media', upload.single('media'), async (req, res) => {
+        try {
+            console.log(req.body);
+            console.log(req.file);
+
+            const { body: { address, id }, file} =  req;
+
+            const uri = await orgids_json.saveMedia('logo', { address, id, file }, baseUrl(req));
+            log.debug('json saved. uri', uri);
+            const json = {
+                data: {
+                    type: 'uri',
                     uri
                 }
             };
