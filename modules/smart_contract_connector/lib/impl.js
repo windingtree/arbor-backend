df6afd03ef345f8501b1b88c5ebf6234556fd1ee
Mathieu TAHON <mathieu.tahon@noath.net>
2020-04-02 18:02:40 +0200

    Refactored the parseOrganization function

--- modules/smart_contract_connector/lib/impl.js
+++ modules/smart_contract_connector/lib/impl.js
@@ -14,13 +14,45 @@ const OrgId = Contracts.getFromNodeModules('@windingtree/org.id', 'OrgId');
 const LifDeposit = Contracts.getFromNodeModules('@windingtree/trust-clue-lif-deposit', 'LifDeposit');
 const LifToken = Contracts.getFromNodeModules('@windingtree/lif-token', 'LifToken');
 
+
 module.exports = function (config, cached) {
-
-    let orgidContract = false;
-    let lifDepositContract = false;
-    let lifTokenContract = false;
+    // Constants
     const orgid0x = '0x0000000000000000000000000000000000000000000000000000000000000000';
+    const { currentEnvironment, environments } = config();
+    const environment = environments[currentEnvironment];
 
+    // Establish websocket and register to events
+    const WSS_URI = `wss://${environment.network}.infura.io/ws/v3/${environment.infuraId}`;
+    var provider = new Web3.providers.WebsocketProvider(WSS_URI);
+    const web3 = new Web3(provider);
+    const registerProviderEvents = (provider => {
+        provider.on('connect', function () {
+            log.debug('WS Connected');
+        });
+
+        // Subscribe to websocket error events
+        provider.on('error', e => {
+            log.debug('WS Connexion Error', e);
+        });
+        
+        // Subscribe to websocket connection errors
+        provider.on('end', e => {
+            log.debug('WS closed', e);
+            log.debug('Attempting to reconnect...');
+            provider = new Web3.providers.WebsocketProvider(WSS_URI);
+            registerProviderEvents(provider);
+            web3.setProvider(provider);
+        });
+    });
+    registerProviderEvents(provider);
+
+
+    // Cached version of the contracts for lazy loading
+    var orgidContract;
+    var lifDepositContract;
+    var lifTokenContract;
+
+    // Get the Organizations in DNS for the DNS Trust Clue
     const getOrgidFromDns = async (link) => {
         return new Promise((resolve) => {
             try {
@@ -39,6 +71,7 @@ module.exports = function (config, cached) {
         })
     };
 
+    // Get the Organizations in URL for the Domain Trust Clue
     const getOrgidFromUrl = async (link) => {
         return new Promise(async (resolve) => {
             try {
@@ -88,16 +121,13 @@ module.exports = function (config, cached) {
         })
     };
 
+    // Get the current environment
     const getEnvironment = () => {
-        const { currentEnvironment, environments } = config();
-        const provider = new Web3.providers.WebsocketProvider(`wss://${environments[currentEnvironment].network}.infura.io/ws/v3/${environments[currentEnvironment].infuraId}`);
-        const web3 = new Web3(provider);
-
-        return Object.assign({}, environments[currentEnvironment], { provider, web3 });
+       return environment;
     };
 
     const getCurrentBlockNumber = async () => {
-        const { web3 } = getEnvironment();
+        //const { web3 } = getEnvironment();
         return new Promise((resolve, reject) => {
             web3.eth.getBlockNumber((err, data) => {
                 if(err) return reject(err);
@@ -106,37 +136,45 @@ module.exports = function (config, cached) {
         });
     };
 
+    // Get the ORG.ID contract
     const getOrgidContract = async () => {
-        if (orgidContract) return orgidContract;
-        const { web3, orgidAddress } = getEnvironment();
-        orgidContract = await OrgId.at(orgidAddress);
-        orgidContract.setProvider(web3.currentProvider);
+        // Check if cached
+        if (!orgidContract) {
+            // Retrieve the instance
+            orgidContract = await OrgId.at(environment.orgidAddress);
+            orgidContract.setProvider(web3.currentProvider);
+        }
+        
         return orgidContract
     };
 
+    // Get the LIF Deposit contract
     const getLifDepositContract = async () => {
-        if (lifDepositContract) return lifDepositContract;
-        const environment = getEnvironment();
-        lifDepositContract = LifDeposit.at(environment.lifDepositAddress);
-        lifDepositContract.setProvider(environment.web3.currentProvider);
+        if (!lifDepositContract) {
+            lifDepositContract = LifDeposit.at(environment.lifDepositAddress);
+            lifDepositContract.setProvider(environment.web3.currentProvider);
+        }
         return lifDepositContract;
     };
 
+    // Get the LIF Token contract
     const getLifTokenContract = async () => {
-        if (lifTokenContract) return lifTokenContract;
-        const environment = getEnvironment();
-        lifTokenContract = LifToken.at(environment.lifTokenAddress);
-        lifTokenContract.setProvider(environment.web3.currentProvider);
+        if (!lifTokenContract) {
+           lifTokenContract = LifToken.at(environment.lifTokenAddress);
+            lifTokenContract.setProvider(environment.web3.currentProvider); 
+        }
         return lifTokenContract;
     };
 
+    // Retrieve ALL organizations
     const getOrganizationsList = async () => {
-        const orgidContract = await getOrgidContract();
+        let orgidContract = await getOrgidContract();
         return orgidContract.methods.getOrganizations().call();
     };
 
+    // Retrieve the details of one organization
     const getOrganization = async (orgid) => {
-        const orgidContract = await getOrgidContract();
+        let orgidContract = await getOrgidContract();
         try {
             return orgidContract.methods.getOrganization(orgid).call();
         } catch (e) {
@@ -145,11 +183,18 @@ module.exports = function (config, cached) {
         }
     };
 
+    // Get the subsidaries of an orgid
     const getSubsidiaries = async (orgid) => {
-        const orgidContract = await getOrgidContract();
-        return orgidContract.methods.getSubsidiaries(orgid).call();
+        let orgidContract = await getOrgidContract();
+        try {
+            return orgidContract.methods.getSubsidiaries(orgid).call();
+        } catch (e) {
+            log.error(`Error during getting getSubsidiaries ${orgid} from smartcontract`);
+            throw e;
+        }
     };
 
+    // Get the ORG.ID from Facebook post
     const getOrgIdFromFacebookPost = (socialUrl) => {
         return new Promise(async (resolve) => {
             try {
@@ -173,6 +218,7 @@ module.exports = function (config, cached) {
         })
     };
 
+    // Get the ORG.ID from Twitter post
     const getOrgIdFromTwitterPost = (socialUrl) => {
         return new Promise(async (resolve) => {
             try {
@@ -191,57 +237,136 @@ module.exports = function (config, cached) {
         })
     };
 
-    const parseOrganization = async (orgid, parentOrganization = false) => {
-        log.debug('[.]', chalk.blue('parseOrganization'), orgid, typeof orgid);
-        const { currentEnvironment, environments } = config();
-        const { lifDecimals, lifMinimumDeposit } = environments[currentEnvironment];
-        const { /*orgId,*/ orgJsonUri, orgJsonHash, parentEntity, owner, director, state, directorConfirmed, deposit } = await getOrganization(orgid);
-        let isSocialFBProved = false, isSocialTWProved = false, isSocialIGProved = false, isSocialLNProved = false;
-        const orgIdLifDepositAmount = parseFloat(`${deposit.substr(0, deposit.length - lifDecimals)}.${deposit.substr(deposit.length - lifDecimals)}`);
-        console.log('const subsidiaries = (parentEntity !== orgid0x) ? [] : await getSubsidiaries(orgid);');
-        const subsidiaries = (parentEntity !== orgid0x) ? [] : await getSubsidiaries(orgid);
-        if (parentEntity !== orgid0x && parentOrganization === false) {
-            parentOrganization = parseOrganization(parentEntity);
+    // Parse the JSON of an organization
+    const parseOrganizationJson = async (orgJsonUri, orgJsonHash) => {
+        // Bypass for localhost issue
+        // @fixme: to be removed
+        if(orgJsonUri.substr(0,22) == 'https://localhost:3333') {
+            orgJsonUri = 'https://staging-api.arbor.fm' + orgJsonUri.substr(22);
+            log.debug(`Replace URI -> ${orgJsonUri}`);
         }
-        // off-chain
+
+
+        // Retrieve off-chain JSON and validate hash
         let jsonContent, orgJsonHashCalculated, isJsonValid, autoCache;
-        process.stdout.write('off-chain... ');
         try {
-            const orgJsonResponse = await fetch(orgJsonUri);
-            process.stdout.write('[READ-OK]\n');
-            const orgJsonText = await orgJsonResponse.text();
+            let orgJsonResponse = await fetch(orgJsonUri);
+            let orgJsonText = await orgJsonResponse.text();
             orgJsonHashCalculated = Web3.utils.keccak256(orgJsonText);
             jsonContent = JSON.parse(orgJsonText);
             autoCache = Web3.utils.keccak256(JSON.stringify(jsonContent, null, 2));
             isJsonValid = (orgJsonHashCalculated === orgJsonHash) || (autoCache === orgJsonHash);
         } catch (e) {
-            process.stdout.write('[ERROR]\n');
+            log.debug('[ERROR]\n');
             log.debug(e.toString());
         }
 
-        if (!jsonContent) throw 'Cannot get jsonContent';
-        if (!isJsonValid) throw `(got hash=${chalk.red(orgJsonHashCalculated === autoCache ? autoCache : `${orgJsonHashCalculated} ~ ${autoCache}`)} BUT expected ${chalk.green(orgJsonHash)}) for uri ${orgJsonUri}`;
-        const orgidType = (typeof jsonContent.legalEntity === 'object') ? 'legalEntity' : (typeof jsonContent.organizationalUnit === 'object' ? 'organizationalUnit' : 'unknown');
-        const directory = orgidType === 'legalEntity' ? 'legalEntity' : _.get(jsonContent, 'organizationalUnit.type', 'unknown');
-        const name = _.get(jsonContent,  orgidType === 'legalEntity' ? 'legalEntity.legalName' : 'organizationalUnit.name', 'Name is not defined');
-        const logo = _.get(jsonContent,  'media.logo', undefined);
-        const parent = (parentEntity !== orgid0x) ? { orgid: parentEntity, name: parentOrganization.name, proofsQty: parentOrganization.proofsQty || 0 } : undefined;
-        const country = _.get(jsonContent, orgidType === 'legalEntity' ? 'legalEntity.registeredAddress.country' : 'organizationalUnit.address.country', '');
-        const contacts = _.get(jsonContent, `${orgidType}.contacts[0]`, {});
+        // Validation of JSON content
+        if (!jsonContent) {
+            log.error('Error Resolving JSON content', e.toString());
+        }
+        if (!isJsonValid) {
+            log.error(`(got hash=${chalk.red(orgJsonHashCalculated === autoCache ? autoCache : `${orgJsonHashCalculated} ~ ${autoCache}`)} BUT expected ${chalk.green(orgJsonHash)}) for uri ${orgJsonUri}`);
+        }
+        return jsonContent;
+    }
+
+    // Parse an organization
+    const parseOrganization = async (orgid, parentOrganization = false) => {
+        log.debug('[.]', chalk.blue('parseOrganization'), orgid, typeof orgid);
+
+        // Get the Organization data from the smart contract
+        let organization = await getOrganization(orgid);
+        log.debug(`Organization Details: ${JSON.stringify(organization)}`);
+        let owner = organization.owner;
+        let director = organization.director;
+        let state = organization.state;
+        let directorConfirmed = organization.directorConfirmed;
+
+        // Retrieve the details on the parent
+        let parent;
+        if (organization.parentEntity !== orgid0x && parentOrganization === false) {
+            // Attempt to retrieve parent
+            try {
+                // Retrieve Parent Organization Details
+                parentOrganization = parseOrganization(organization.parentEntity);
+
+                // Retrieve parent details
+                parent = {
+                    orgid: organization.parentEntity,
+                    name: parentOrganization.name,
+                    proofsQty: parentOrganization.proofsQty || 0
+                }
+            }
+
+            // In case of error, just log but do not prevent the rest of the resolution
+            catch (e) {
+                log.error('Error Resolving parent', e.toString());
+            }
+
+        }
+
+        // Retrieve the subsidiaries
+        let subsidiaries = [];
+        if(organization.parentEntity !== orgid0x) {
+            subsidiaries = await getSubsidiaries(orgid);
+        }
+
+        // Retrieve the offchain data
+        let jsonContent= parseOrganizationJson(organization.orgJsonUri, organization.orgJsonHash);
+        if(!jsonContent) {
+            log.error('Error Resolving JSON content', e.toString());
+            throw("Organization resolution aborted due to error retrieving JSON content"); 
+        }
+
+        // Get relevant details from offchain data
+        let orgidType = (typeof jsonContent.legalEntity === 'object') ? 'legalEntity' : (typeof jsonContent.organizationalUnit === 'object' ? 'organizationalUnit' : 'unknown');
+        let directory = orgidType === 'legalEntity' ? 'legalEntity' : _.get(jsonContent, 'organizationalUnit.type', 'unknown');
+        let name = _.get(jsonContent,  orgidType === 'legalEntity' ? 'legalEntity.legalName' : 'organizationalUnit.name', 'Name is not defined');
+        let logo = _.get(jsonContent,  'media.logo', undefined);
+        let country = _.get(jsonContent, orgidType === 'legalEntity' ? 'legalEntity.registeredAddress.country' : 'organizationalUnit.address.country', '');
+        let contacts = _.get(jsonContent, `${orgidType}.contacts[0]`, {});
+
+
+        // Check the LIF deposit amount
+        //const orgIdLifDepositAmount = parseFloat(`${organization.deposit.substr(0, organization.deposit.length - lifDecimals)}.${organization.deposit.substr(organization.deposit.length - lifDecimals)}`);
+        let lifDeposit = web3.utils.fromWei(organization.deposit, 'ether');
+        let isLifProved =  lifDeposit >= environment.lifMinimumDeposit;
+
+        // Facebook Trust clue
+        let isSocialFBProved = false;
         const trustFacebookUri = _.get(_.filter(_.get(jsonContent, `trust`, []), (clue) => ['social', 'facebook'].indexOf(clue.type) !== -1 && clue.proof.indexOf('facebook') !== -1), '[0].proof', false);
         if (trustFacebookUri) {
             isSocialFBProved =  (await getOrgIdFromFacebookPost(trustFacebookUri)) === orgid;
         }
+        
+        // Twitter Trust clue
+        let isSocialTWProved = false;
         const trustTwitterUri = _.get(_.filter(_.get(jsonContent, `trust`, []), (clue) => ['social', 'twitter'].indexOf(clue.type) !== -1 && clue.proof.indexOf('twitter') !== -1), '[0].proof', false);
         if (trustTwitterUri) {
             isSocialTWProved =  (await getOrgIdFromTwitterPost(trustFacebookUri)) === orgid;
         }
+
+        // Instagram Trust clue
+        // @fixme to be implemented
+        let isSocialIGProved = false;
+
+        // Linkedin Trust clue
+        // @fixme to be implemented
+        let isSocialLNProved = false;
+
+        // Overall Social Trust proof
+        let isSocialProved = isSocialFBProved || isSocialTWProved || isSocialIGProved || isSocialLNProved;
+
+        // Website Trust clue
         const {website} = contacts;
         const isWebsiteProved = (orgid === (await getOrgidFromLink(website)));
+
+        // SSL Trust clue
         let isSslProved = false;
         if (isWebsiteProved) isSslProved = checkSslByUrl(website);
-        let isLifProved =  orgIdLifDepositAmount >= lifMinimumDeposit;
-        const isSocialProved = isSocialFBProved || isSocialTWProved || isSocialIGProved || isSocialLNProved;
+        
+        // Retrurn all details
         return {
             orgid,
             owner,
@@ -262,9 +387,9 @@ module.exports = function (config, cached) {
             isSocialFBProved,
             isSocialTWProved,
             isSocialIGProved,
-            isJsonValid,
-            orgJsonHash,
-            orgJsonUri,
+            //isJsonValid,
+            //orgJsonHash,
+            //orgJsonUri,
             jsonContent,
             jsonCheckedAt: new Date().toJSON(),
             jsonUpdatedAt: new Date().toJSON()
@@ -335,13 +460,31 @@ module.exports = function (config, cached) {
     const listenEvents = async () => {
         try {
             const orgidContract = await getOrgidContract();
-            const currentBlockNumber = await getCurrentBlockNumber();
+            let currentBlockNumber = await getCurrentBlockNumber();
+            log.debug(`Subscribing to events for Orgid Contract ${orgidContract.options.address}`);
             log.debug(`event listening started...${chalk.grey(`(from block ${currentBlockNumber})`)}`);
-            orgidContract.events
-                .allEvents({ fromBlock: currentBlockNumber - 10 /* -10 in case of service restart*/ }, async (/*error, event*/) => {})
-                .on('data', resolveOrgidEvent)
-                .on('changed', (event) => log.debug("=================== Changed ===================\r\n", event))
-                .on('error', (error) => log.debug("=================== ERROR ===================\r\n", error));
+            
+            // Start Listening on all Events
+            orgidContract.events.allEvents({ 
+                fromBlock: currentBlockNumber - 10 /* -10 in case of service restart*/ 
+            }, (error, event) => {
+
+                // Callback for new errors or events
+                if(error) log.debug(`Error: ${error}`);
+                if(event) log.debug(`Event: ${event}`);
+            })
+
+            // Connection established
+            .on('connected', (subscriptionId) => {log.debug(`Connected with ${subscriptionId}`);})
+
+            // Event Received
+            .on('data', resolveOrgidEvent)
+
+            // Change event
+            .on('changed', (event) => log.debug("=================== Changed ===================\r\n", event))
+
+            // Error Event
+            .on('error', (error) => log.debug("=================== ERROR ===================\r\n", error));
         } catch (e) {
             log.error('Error during listenEvents', e.toString());
         }
