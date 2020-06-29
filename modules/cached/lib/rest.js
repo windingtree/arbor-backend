bb2432b154cc5ca76b2f259bf3ae540356aed7a2
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-06-29 15:12:58 +0300

    update email stype

--- modules/cached/lib/rest.js
+++ modules/cached/lib/rest.js
@@ -83,10 +83,126 @@ module.exports = function (rest, cached) {
                 to: req.body.email,
                 from: 'noreply@windingtree.com',
                 subject: 'Your Profile Draft on WindingTree marketplace',
-                text: `Your Profile Id: ${profileId}; password for updates: ${password}\n
-                    Please follow the link to create an ORGiD: https://staging.arbor.fm/my-organizations?profileId=${profileId}`,
-                html: `Your Profile Id: ${profileId}; password for updates: ${password}\n
-                    Please follow the link to create an ORGiD: https://staging.arbor.fm/my-organizations?profileId=${profileId}`
+                text: `Your organization is almost created!\n\n
+                    We've saved your organization and created a draft of it on the Winding Tree Marketplace.\n
+                    Now you need a desktop web browser with installed a MetaMask extension to continue creating a digital identity for your organisation.\n\n
+                    https://${environment.network === 'ropsten' ? 'staging.arbor.fm' : 'arbor.fm'}/my-organizations?profileId=${profileId}`,
+                html: `<!DOCTYPE html
+                PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+            <html xmlns="http://www.w3.org/1999/xhtml">
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                <title>Your organization is almost created</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <style>
+                    body {
+                        margin: 0;
+                        padding: 0;
+                        background-color: white;
+                        font-style: normal;
+                        font-weight: 500;
+                        font-size: 16px;
+                        line-height: 28px;
+                        color: #5E666A;
+                    }
+                    .content {
+                        margin-top: 52px;
+                    }
+                    .pad {
+                        width: auto;
+                    }
+                    .center {
+                        width: 600px;
+                        max-width: 600px;
+                    }
+                    .line {
+                        width: 100%;
+                        border-bottom: 2px solid #E3F9EB;
+                    }
+                    .logo {
+                        
+                    }
+                    h1 {
+                        font-style: normal;
+                        font-weight: 500;
+                        font-size: 40px;
+                        line-height: 52px;
+                        color: #42424F;
+                    }
+                    p:first-child {
+                        margin-top: 52px;
+                    }
+                    p:last-child {
+                        margin-bottom: 52px;
+                    }
+                    .button {
+                        display: flex;
+                        flex-direction: row;
+                        padding: 10px 20px;
+                        border: 2px solid #3F4244;
+                        box-sizing: border-box;
+                        border-radius: 8px;
+                        width: 217px;
+                        height: 44px;
+                        left: 80px;
+                        top: 509px;
+                        font-family: Raleway;
+                        font-style: normal;
+                        font-weight: 600;
+                        font-size: 16px;
+                        line-height: 24px;
+                        color: #3F4244;
+                    }
+                    a {
+                        text-decoration: none;
+                    }
+                </style>
+            </head>
+            <body>
+                <table class="content" border="0" cellpadding="0" cellspacing="0" width="100%">
+                    <tr>
+                        <td class="pad">&nbsp;</td>
+                        <td class="center">
+                            <div class="logo">
+                                <img src="https://staging.arbor.fm/wlogo.png" />
+                            </div>          
+                        </td>
+                        <td class="pad">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <h1>Your organization is almost created</h1>
+                            <div class="line"></div>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <p>
+                                We've saved your organization and created a draft of it on the Winding Tree Marketplace.
+                            </p>
+                            <p>
+                                Now you need a desktop web browser with installed a MetaMask extension to continue creating a digital identity for your organisation.
+                            </p>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <a href="https://${environment.network === 'ropsten' ? 'staging.arbor.fm' : 'arbor.fm'}/my-organizations?profileId=${profileId}" target="_blank">
+                                <div class="button">
+                                    Go to My Organization
+                                </div>
+                            </a>
+                        </td>
+                        <td></td>
+                    </tr>
+                </table>
+            </body>
+            </html>`
             };
             await sgMail.send(msg);
             res.status(200).json({
