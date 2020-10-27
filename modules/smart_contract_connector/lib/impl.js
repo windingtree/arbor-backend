3030474ba489a10102d79bd91a647de207910978
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-10-27 13:30:30 +0200

    sending emails on directories events

--- modules/smart_contract_connector/lib/impl.js
+++ modules/smart_contract_connector/lib/impl.js
@@ -1,6 +1,7 @@
 const Web3 = require('web3');
 const _ = require('lodash');
 const chalk = require('chalk');
+const sgMail = require('@sendgrid/mail');
 const {
     waitForBlockNumber,
     createResolver,
@@ -25,6 +26,7 @@ const orgid0x = '0x0000000000000000000000000000000000000000000000000000000000000
 module.exports = (config, cached) => {
     const { currentEnvironment, environments } = config();
     const environment = environments[process.env.NODE_ENV === 'dev' ? 'development' : currentEnvironment];
+    sgMail.setApiKey(environment.sendgridApiKey);
 
     let web3;
     let orgIdResolver;
@@ -63,27 +65,233 @@ module.exports = (config, cached) => {
         }
     );
 
-    // Queue for promises
-    const queue = new SimpleQueue();
+    const sendEmail = async (
+        email,
+        title,
+        text,
+        link
+    ) => {
+        const basePath = environment.network === 'ropsten' ? 'staging.arbor.fm' : 'marketplace.windingtree.com';
+        const msg = {
+            to: email,
+            from: 'noreply@windingtree.com',
+            subject: title,
+            text: `${title}\n\n
+                ${text}\n\n
+                ${link}`,
+            html: `<!DOCTYPE html
+            PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            <title>${title}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <style>
+                body {
+                    font-family: Arial, Helvetica, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    background-color: white;
+                    font-style: normal;
+                    font-weight: 500;
+                    font-size: 16px;
+                    line-height: 28px;
+                    color: #5E666A;
+                }
+                .content {
+                    margin-top: 52px;
+                }
+                .pad {
+                    width: auto;
+                }
+                .center {
+                    width: 600px;
+                    max-width: 600px;
+                }
+                .line {
+                    width: 100%;
+                    border-bottom: 2px solid #E3F9EB;
+                }
+                .logo {
+
+                }
+                h1 {
+                    font-family: Arial, Helvetica, sans-serif;
+                    font-style: normal;
+                    font-weight: 500;
+                    font-size: 32px;
+                    line-height: 52px;
+                    color: #42424F;
+                }
+                .first {
+                    margin-top: 52px;
+                }
+                .last {
+                    margin-bottom: 52px;
+                }
+                .button {
+                    display: flex;
+                    flex-direction: row;
+                    padding: 10px 20px;
+                    border: 2px solid #3F4244;
+                    box-sizing: border-box;
+                    border-radius: 8px;
+                    width: 217px;
+                    height: 44px;
+                    left: 80px;
+                    top: 509px;
+                    font-family: Arial, Helvetica, sans-serif;
+                    font-style: normal;
+                    font-weight: 600;
+                    font-size: 16px;
+                    line-height: 24px;
+                    color: #3F4244;
+                }
+                a {
+                    text-decoration: none;
+                }
+            </style>
+        </head>
+        <body>
+            <table class="content" border="0" cellpadding="0" cellspacing="0" width="100%">
+                <tr>
+                    <td class="pad">&nbsp;</td>
+                    <td class="center">
+                        <div class="logo">
+                            <img src="https://${basePath}/wlogo.png" />
+                        </div>
+                    </td>
+                    <td class="pad">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <h1>${title}</h1>
+                        <div class="line"></div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        ${text}
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <a href="${basePath}/${link}" target="_blank">
+                            <div class="button">
+                            ${basePath}/${link}
+                            </div>
+                        </a>
+                    </td>
+                    <td></td>
+                </tr>
+            </table>
+        </body>
+        </html>`
+        };
+        await sgMail.send(msg);
+    };
+
+    const getOrgEmail = async orgId => {
+        const organization = await cached.getOrgId(orgId);
+        let contacts;
+        let email;
+        if (organization.orgidType === 'legalEntity') {
+            contacts = organization.jsonContent.legalEntity &&
+                organization.jsonContent.legalEntity.contacts &&
+                organization.jsonContent.legalEntity.contacts.length > 0
+                ? organization.jsonContent.legalEntity.contacts
+                : null;
+        } else if (organization.orgidType === 'organizationalUnit') {
+            contacts = organization.jsonContent.organizationalUnit &&
+                organization.jsonContent.organizationalUnit.contacts &&
+                organization.jsonContent.organizationalUnit.contacts.length > 0
+                ? organization.jsonContent.organizationalUnit.contacts
+                : null;
+        }
+
+        if (contacts) {
+            const emailContact = contacts.filter(c => c.email)[0];
+            email = emailContact ? emailContact.email : undefined;
+        }
+
+        return email;
+    };
 
     const startDirsSubscriptions = async (web3, directoryIndexAddress) => {
-        return;// @todo REMOVE after new directories feature will be enabled
         try {
             const directories = await fetchDirectoriesIndex(web3, directoryIndexAddress);
-            return subscribeDirectoriesEvents(web3, 'latest', directories, event => {
-                switch (event.event) {
-                    case 'OrganizationChallenged':
-                    case 'Dispute':
-                        // Send email to organization owner
-                        break;
-                    default:
-                        // Ignore all other events
+            let email;
+            return subscribeDirectoriesEvents(web3, 'latest', directories, async event => {
+                try {
+                    switch (event.event) {
+                        case 'OrganizationChallenged':
+                            // Send email to organization owner
+                            email = await getOrgEmail(event.event.returnValues._organization);
+                            if (email) {
+                                await sendEmail(
+                                    email,
+                                    'Organization has been challenged',
+                                    'Your organization has been challenged. You can see details and accept on the organization page.'
+                                    `organization/${event.event.returnValues._organization}`
+                                );
+                            }
+                            break;
+                        case 'OrganizationAdded':
+                            // Send email to organization owner
+                            email = await getOrgEmail(event.event.returnValues._organization);
+                            if (email) {
+                                await sendEmail(
+                                    email,
+                                    'Organization has been added to the directory',
+                                    `Your organization has been added to the directory ${event.event.address}. You can see details and accept on the organization page.`,
+                                    `organization/${event.event.returnValues._organization}`
+                                );
+                            }
+                            break;
+                        case 'OrganizationRemoved':
+                            // Send email to organization owner
+                            email = await getOrgEmail(event.event.returnValues._organization);
+                            if (email) {
+                                await sendEmail(
+                                    email,
+                                    'Organization has been removed from the directory',
+                                    `Your organization has been removed from the directory ${event.event.address}. You can see details and accept on the organization page.`,
+                                    `organization/${event.event.returnValues._organization}`
+                                );
+                            }
+                            break;
+                        case 'OrganizationRequestRemoved':
+                            // Send email to organization owner
+                            email = await getOrgEmail(event.event.returnValues._organization);
+                            if (email) {
+                                await sendEmail(
+                                    email,
+                                    'Organization adding request has been removed',
+                                    `Your request to the directory ${event.event.address} has been removed.`,
+                                    `directories/requests/${event.event.address}`
+                                );
+                            }
+                            break;
+                        case 'Evidence':
+                        default:
+                            // Ignore all other events
+                    }
+                } catch (error) {
+                    log.error(error);
                 }
             });
         } catch (error) {
             log.error('Error during startDirsSubscriptions', error.toString());
         }
     };
+
+    // Queue for promises
+    const queue = new SimpleQueue();
 
     // Start Listening on all OrgId contract Events
     const listenEvents = async (web3, orgidContract, orgIdResolver) => {
